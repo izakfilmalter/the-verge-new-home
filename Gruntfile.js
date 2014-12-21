1230cf36781ca80f7629fe3fe765566c007442a0
Izak Filmalter <izak@moderninc.io>
2014-12-20 22:58:37 -0800

    Moved normalize into elements. Worked on function for creating lists of extends.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,32 @@ module.exports = function(grunt) {
           style: 'expanded'
         },
         files: {
-          'public/css/style-dev.css': 'sass/style-development.sass'
+          'src/style.css': 'sass/style-development.sass'
+        }
+      }
+    },
+
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions'],
+        map: true,
+      },
+      your_target: {
+        src: 'src/style.css',
+        dest: 'public/css/pre.css'
+      },
+    },
+
+    cssc: {
+      csscFirstSet: {
+        options: {
+          consolidateViaDeclarations: true,
+          consolidateViaSelectors: true,
+          consolidateMediaQueries: true,
+          sort: true
+        },
+        files: {
+          'public/css/style.css' : 'public/css/pre.css'
         }
       }
     },
@@ -26,7 +51,7 @@ module.exports = function(grunt) {
           'sass/**/**/**/*.scss',
           'sass/**/**/**/**/*.scss',
         ],
-        tasks: ['sass']
+        tasks: ['sass', 'autoprefixer']
       },
       php: {
         files: [
@@ -43,6 +68,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-cssc');
 
   grunt.registerTask('default', ['watch']);
 
--- public/components/head/head.php
+++ public/components/head/head.php
@@ -1,2 +1,2 @@
 <link href="/css/fonts.css" media="all" rel="stylesheet" type="text/css">
-<link href="/css/style-dev.css" media="all" rel="stylesheet" type="text/css">
+<link href="/css/style.css" media="all" rel="stylesheet" type="text/css">
